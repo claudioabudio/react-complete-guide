07e97db02da0013962b8f307f3033b82995f396d
claudio Abud <clau@gmail.com>
2021-06-27 15:27:41 -0500

    use javascript expressions within JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021,2,15)
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 350
     return (
         <div className="expense-item">
-            <div>March 28th 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$350</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     )
