a870e5d8ea20dab28ba912e4898d565dd0f35a37
claudio Abud <clau@gmail.com>
2021-06-27 21:38:32 -0500

    feature based packaging of components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
 
