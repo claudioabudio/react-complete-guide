9d264727c4e1492f85a82a96c8dc3e5484f11700
claudio Abud <clau@gmail.com>
2021-07-03 17:43:47 -0500

    Added onSubmit() handler

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -17,12 +17,21 @@ const ExpenseForm = () => {
     }
 
     const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+    }
 
-
+    const onSubmitHandler = (event) => {
+        event.preventDefault()
+        const expenseData = {
+            title : enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(expenseData)
     }
 
     return (
-        <form>
+        <form onSubmit={onSubmitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
