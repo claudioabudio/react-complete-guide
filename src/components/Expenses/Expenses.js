f945edb36f8e7040a70762ee296f935278f47f67
claudio Abud <clau@gmail.com>
2021-07-04 15:38:38 -0500

    displaying an array of expenseItem components using map()

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,21 +15,11 @@ function Expenses(props) {
         return (        
           <Card className="expenses">
             <ExpensesFilter selected={year} onChangeFilter={filterExpenseHandler}/>
-            <ExpenseItem
-              title={props.expenses[0].title}
-              date={props.expenses[0].date}
-              amount={props.expenses[0].amount}
-            />
-            <ExpenseItem
-              title={props.expenses[1].title}
-              date={props.expenses[1].date}
-              amount={props.expenses[1].amount}
-            />
-            <ExpenseItem
-              title={props.expenses[2].title}
-              date={props.expenses[2].date}
-              amount={props.expenses[2].amount}
-            />
+            {props.expenses.map(expense => <ExpenseItem
+               title={expense.title}
+               date={expense.date}
+               amount={expense.amount}
+            />)} 
           </Card>
         );
 
