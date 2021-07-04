8f90c5fa413ce76a37f6981c152fb090f2165d90
claudio Abud <clau@gmail.com>
2021-07-04 16:41:10 -0500

    adds new expense to the list of expenses

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+import {useState} from 'react'
 
-const App = () => {
+const expenses = [
+  {id: '1', date: new Date(2021,3,1), title: 'Car Insurance', amount: 350},
+  {id: '2', date: new Date(2020,3,1), title: 'New apartment', amount: 150000 },
+  {id: '3', date: new Date(2021,3,1), title: 'Rent', amount: 900}
+]
 
-  const expenses = [
-    {id: '1', date: new Date(2021,3,1), title: 'Car Insurance', amount: 350},
-    {id: '2', date: new Date(2020,3,1), title: 'New apartment', amount: 150000 },
-    {id: '3', date: new Date(2021,3,1), title: 'Rent', amount: 900}
-  ]
+const App = () => { 
+
+  const [expensesList, setExpensesList] = useState(expenses)
 
   const addExpenseHandler = (expense) => {
       console.log("In App.js")
       console.log(expense)
+      setExpensesList((prevExpensesList, props) => [...prevExpensesList, expense])
   }
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses expenses={expenses}/>
+      <Expenses expenses={expensesList}/>
     </div>
   );
 }
