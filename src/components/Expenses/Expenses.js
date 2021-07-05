3058e4d3e97afaa175e7a47fd980c982b25339b7
claudio Abud <clau@gmail.com>
2021-07-04 22:55:39 -0500

    filtered list of expenses based on the year selected

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,15 +7,18 @@ import {useState} from 'react'
 function Expenses(props) {
         const [year, setYear] = useState('2020')
 
+        const [filteredExpenses, setFilteredExpenses] = useState(props.expenses)
+
         const filterExpenseHandler = (year) => {
             setYear(year)
-            console.log(year)
-        }
+            setFilteredExpenses(props.expenses.filter(expense => 
+                  expense.date.getFullYear().toString() === year))
+          }
 
         return (        
           <Card className="expenses">
             <ExpensesFilter selected={year} onChangeFilter={filterExpenseHandler}/>
-            {props.expenses.map(expense => <ExpenseItem
+            {filteredExpenses.map(expense => <ExpenseItem
                key={expense.id}
                title={expense.title}
                date={expense.date}
