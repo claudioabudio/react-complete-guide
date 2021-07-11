f88e9ec16f4aaf64ef5562c18a1d72db3a2d6e1d
claudio Abud <clau@gmail.com>
2021-07-11 17:15:22 -0500

    Added a Bar Chart to chart the expenses per month

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import './Expenses.css';
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
 import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
 import {useState} from 'react'
 
 function Expenses(props) {
@@ -17,6 +18,7 @@ function Expenses(props) {
         return (        
           <Card className="expenses">
             <ExpensesFilter selected={year} onChangeFilter={filterExpenseHandler}/>
+            <ExpensesChart expenses={filteredExpenses}></ExpensesChart>
             <ExpensesList items={filteredExpenses}/>
           </Card>
         );
